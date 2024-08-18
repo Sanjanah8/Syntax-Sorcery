4e85ca6e29be426172df6bcbf6c77b5cbaa5d13a
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-19 00:01:47 +0530

    Create Simple Calculator app.dart

---
+++ Dart/Apps(Flutter)/Simple Calculator app.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CalculatorHomePage(),
+    );
+  }
+}
+
+class CalculatorHomePage extends StatefulWidget {
+  @override
+  _CalculatorHomePageState createState() => _CalculatorHomePageState();
+}
+
+class _CalculatorHomePageState extends State<CalculatorHomePage> {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "/" ||
+        buttonText == "x") {
+      num1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      } else if (operand == "-") {
+        _output = (num1 - num2).toString();
+      } else if (operand == "x") {
+        _output = (num1 * num2).toString();
+      } else if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: OutlineButton(
+        padding: EdgeInsets.all(24.0),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () => buttonPressed(buttonText),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Simple Calculator')),
+      body: Column(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+              child: Text(output, style: TextStyle(fontSize: 48.0))),
+          Expanded(child: Divider()),
+          Column(children: [
+            Row(children: [
+              buildButton("7"),
+              buildButton("8"),
+              buildButton("9"),
+              buildButton("/")
+            ]),
+            Row(children: [
+              buildButton("4"),
+              buildButton("5"),
+              buildButton("6"),
+              buildButton("x")
+            ]),
+            Row(children: [
+              buildButton("1"),
+              buildButton("2"),
+              buildButton("3"),
+              buildButton("-")
+            ]),
+            Row(children: [
+              buildButton("."),
+              buildButton("0"),
+              buildButton("00"),
+              buildButton("+")
+            ]),
+            Row(children: [
+              buildButton("CLEAR"),
+              buildButton("="),
+            ])
+          ])
+        ],
+      ),
+    );
+  }
+}
