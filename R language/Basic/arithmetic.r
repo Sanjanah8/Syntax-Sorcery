7899996ec94c0ce90a28b7e4008dac29f31fce07
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-19 00:06:50 +0530

    Create arithmetic.r

---
+++ R language/Basic/arithmetic.r
@@ -0,0 +1,14 @@
+# Basic Arithmetic Operations
+a <- 10
+b <- 5
+
+sum <- a + b
+difference <- a - b
+product <- a * b
+quotient <- a / b
+
+# Print results
+print(paste("Sum:", sum))
+print(paste("Difference:", difference))
+print(paste("Product:", product))
+print(paste("Quotient:", quotient))
