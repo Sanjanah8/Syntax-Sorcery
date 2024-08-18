e1062e0d1c62d2db2d897f78823b1b72666b9419
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-18 23:54:22 +0530

    Create simple calculator.dart

---
+++ Dart/Basic/simple calculator.dart
@@ -0,0 +1,8 @@
+void main() {
+  int a = 10;
+  int b = 5;
+  print('Addition: ${a + b}');
+  print('Subtraction: ${a - b}');
+  print('Multiplication: ${a * b}');
+  print('Division: ${a / b}');
+}
