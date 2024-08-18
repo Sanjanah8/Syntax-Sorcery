46043dfa0f4f3756a7f963d523bfcecf376b167c
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-19 00:00:10 +0530

    Create simple calculator.go

---
+++ Go/Basic/simple calculator.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := 10
+  b := 5
+  fmt.Println("Addition:", a+b)
+  fmt.Println("Subtraction:", a-b)
+  fmt.Println("Multiplication:", a*b)
+  fmt.Println("Division:", a/b)
+}
