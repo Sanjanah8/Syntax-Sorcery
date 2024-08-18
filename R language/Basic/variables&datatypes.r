478b796fd943b164ea1f451abfacf86f22e8f4b5
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-19 00:07:14 +0530

    Create variables&datatypes.r

---
+++ R language/Basic/variables&datatypes.r
@@ -0,0 +1,11 @@
+# Variables and Data Types
+integer_var <- 42       # Integer
+double_var <- 3.14      # Double (Float)
+char_var <- "Hello R!"  # Character String
+logical_var <- TRUE     # Logical (Boolean)
+
+# Print variable types
+print(paste("Integer Variable:", integer_var))
+print(paste("Double Variable:", double_var))
+print(paste("Character Variable:", char_var))
+print(paste("Logical Variable:", logical_var))
