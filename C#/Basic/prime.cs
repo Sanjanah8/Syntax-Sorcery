f7340fdc569789e0f94bca8857c5833b1d365505
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-28 15:41:12 +0530

    Create prime.cs

---
+++ C#/Basic/prime.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        bool isPrime = true;
+
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine(number + " is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a prime number.");
+        }
+    }
+}
