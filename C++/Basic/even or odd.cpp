a1cd20def41edb820d7a615c6605f49f6dc77343
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-28 15:45:46 +0530

    Create even or odd.cpp

---
+++ C++/Basic/even or odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (number % 2 == 0) {
+        std::cout << number << " is even." << std::endl;
+    } else {
+        std::cout << number << " is odd." << std::endl;
+    }
+    return 0;
+}
